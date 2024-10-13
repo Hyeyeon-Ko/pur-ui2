9217b1ecaf274a5095f287e438194b488e6660e2
gaia <gaia@kmi.or.kr>
2024-10-14 07:52:29 +0900

    Feat: layout추가

---
+++ app/(table)/layout.tsx
@@ -0,0 +1,30 @@
+// import Image from "next/image";
+import SideMenu from "@/components/layouts/SideMenu";
+import Link from "next/link";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="h-screen flex">
+      {/* left */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <Link
+          href="/"
+          className="flex items-center justify-center gap-2 lg:justify-start"
+        >
+          {/* <Image src="" alt="logo" width={32} height={32} /> */}
+          <span className="hidden lg:block">side menu</span>
+        </Link>
+        <SideMenu />
+      </div>
+      {/* right */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+        {/* <Navbar /> */}
+        {children}
+      </div>
+    </div>
+  );
+}
--- app/main/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>main - id</div>
-  )
-}
-
-export default page
