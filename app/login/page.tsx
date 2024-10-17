d8cc0c4fa170135246f04b71397206c027d39853
gaia <gaia@kmi.or.kr>
2024-10-18 07:59:34 +0900

    Feat: 모달 컴포넌트 생성

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,12 +2,36 @@
 
 import Button from "@/components/ui/atoms/button/Button";
 import Table from "@/components/ui/molecules/table/Table";
+import Modal from "@/components/ui/organism/modal/Modal";
 import useExcelFileHandler from "@/hooks/useExcelFileHandler";
+import useFormatHandler from "@/hooks/useFormatHandler";
+import useModal from "@/hooks/useModal";
 import { columns, data } from "@/lib/data";
 import React, { useRef, useState } from "react";
 
 const LoginPage = () => {
   const [selectedRows, setSelectedRows] = useState<string[]>([]);
+  const { isOpen, openModal, closeModal } = useModal();
+
+  const { formatCenterData, formatDate, formatCurrency } = useFormatHandler();
+
+  const formattedData = data.map((item) => ({
+    ...item,
+    센터: formatCenterData(item.센터) || "-",
+    공고일: formatDate(item.공고일),
+    마감일: formatDate(item.마감일),
+    응찰일: formatDate(item.응찰일),
+    낙찰기준가: formatCurrency(item.낙찰기준가),
+    낙찰금액: formatCurrency(item.낙찰금액),
+    열람: `<button>${item.열람}</button>`,
+    누리장터: item.누리장터 || "-",
+  }));
+
+  const handleConfirm = () => {
+    // 확인 버튼 클릭 시 실행할 로직
+    console.log("확인 버튼 클릭됨");
+    closeModal(); // 모달 닫기
+  };
   const { handleFileUpload, handleFileDownload } = useExcelFileHandler(
     data,
     selectedRows
@@ -29,13 +53,17 @@ const LoginPage = () => {
       const selectedData = selectedRows
         .map((rowId) => {
           const rowData = data.find((row) => row.id === rowId);
-          // 각각 포함된 rowData의 값
-          return rowData ? Object.values(rowData) : null;
+          if (rowData) {
+            return Object.values(rowData).map((value) => {
+              return Array.isArray(value) ? value.join(", ") : value;
+            });
+          }
+          return null;
         })
         .filter((row) => row !== null);
 
       if (selectedData.length > 0) {
-        handleFileDownload(selectedData);
+        handleFileDownload(selectedData as string[][]);
       } else {
         alert("선택된 데이터가 없습니다.");
       }
@@ -46,6 +74,18 @@ const LoginPage = () => {
 
   return (
     <>
+      <div>
+        <Button mode="lg" content="modal" onClick={openModal} />
+        <Modal
+          isOpen={isOpen}
+          closeModal={closeModal}
+          title="진짜제목"
+          onCancelClick={handleConfirm}
+          onConfirmClick={handleConfirm}
+        >
+          <p>모달 내부에 추가적인 내용</p>
+        </Modal>
+      </div>
       <div className="flex justify-end mr-6 mt-10">
         <input
           type="file"
@@ -69,7 +109,7 @@ const LoginPage = () => {
         />
       </div>
       <Table
-        data={data}
+        data={formattedData}
         columns={columns}
         onRowSelect={handleRowSelect}
         showCheckbox={true}
---
+++ components/ui/organism/modal/Modal.tsx
@@ -0,0 +1,138 @@
+import React, { CSSProperties, useCallback, useEffect, useRef } from "react";
+import colors from "@/styles/colors";
+import Button from "../../atoms/button/Button";
+import Label from "../../atoms/label/Label";
+
+/**
+ *isOpen: 모달 열림 상태
+  closeModal: 모달 닫기 함수
+  mode: 모달 크기
+  color: 색상
+  customStyle: CSSProperties;
+  title: 제목
+  titleFontSize: 제목 폰트사이즈
+  showCancelButton: 취소 버튼을 표시할지 여부
+  showConfirmButton: 확인 버튼을 표시할지 여부
+  cancelText: 취소 버튼 텍스트
+  confirmText: 확인 버튼 텍스트
+  onCancelClick: 취소 버튼 클릭 핸들러
+  onConfirmClick: 확인 버튼 클릭 핸들러
+  children: React.ReactNode;
+ */
+export type ModalMode = "sm" | "xs" | "lg" | "md" | undefined;
+
+interface ModalProps {
+  isOpen: boolean;
+  closeModal: () => void;
+  mode?: ModalMode;
+  color?: keyof typeof colors;
+  customStyle?: CSSProperties;
+  title?: string;
+  titleFontSize?: string;
+  showCancelButton?: boolean;
+  showConfirmButton?: boolean;
+  cancelText?: string;
+  confirmText?: string;
+  onCancelClick?: () => void;
+  onConfirmClick?: () => void;
+  children: React.ReactNode;
+}
+
+const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  closeModal,
+  mode = "md",
+  color,
+  customStyle,
+  title,
+  titleFontSize = "1.25rem",
+  showCancelButton = true,
+  showConfirmButton = true,
+  cancelText = "취소",
+  confirmText = "확인",
+  onCancelClick,
+  onConfirmClick,
+  children,
+}) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  const handleOutsideClick = useCallback(
+    (e: MouseEvent) => {
+      if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
+        closeModal();
+      }
+    },
+    [closeModal]
+  );
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener("mousedown", handleOutsideClick);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [isOpen, handleOutsideClick]);
+
+  if (!isOpen) return null;
+
+  const modeClasses = {
+    sm: "text-sm px-4 py-2",
+    xs: "text-xs px-2 py-1",
+    md: "text-base px-5 py-2.5",
+    lg: "text-lg px-6 py-3",
+  };
+
+  const colorClass = color ? colors[color] : "";
+
+  return (
+    <div
+      style={{ ...customStyle }}
+      className="fixed inset-0 flex items-center justify-center p-20"
+    >
+      <div
+        style={{ backgroundColor: colors.black, opacity: 0.5 }}
+        className="absolute inset-0"
+      />
+
+      <div
+        style={{ backgroundColor: colors.white }}
+        ref={modalRef}
+        className={`relative bg-white rounded shadow-lg z-10 pt-6 ${modeClasses[mode]} ${colorClass}`}
+      >
+        <div className="flex w-[100%] justify-between items-center pb-6">
+          {title && (
+            <Label
+              mode="lg"
+              fontWeight="bold"
+              content={title}
+              customStyle={{ fontSize: titleFontSize }}
+            />
+          )}
+          <button onClick={closeModal} className="text-gray-500 text-2xl">
+            &times;
+          </button>
+        </div>
+        {/* 모달 컨텐츠가 들어가는 곳 */}
+        {children}
+
+        <div className="flex justify-end mt-4 pt-12 pb-2">
+          {showCancelButton && (
+            <Button
+              mode="sm"
+              onClick={onCancelClick || closeModal}
+              variant="outline"
+              color="Button_Default"
+              content={cancelText}
+            />
+          )}
+          {showConfirmButton && (
+            <Button mode="sm" onClick={onConfirmClick} content={confirmText} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
