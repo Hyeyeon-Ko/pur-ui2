794cda9e3d3d17d3d91036ad96d1ce99c80b77c2
gaia <gaia@kmi.or.kr>
2024-10-24 07:16:50 +0900

    Feat: 404페이지 ui 생성

---
+++ app/not-found.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React from "react";
+import { useRouter } from "next/navigation";
+import Button from "@/components/ui/atoms/button/Button";
+import colors from "@/styles/colors";
+import { FaExclamationTriangle } from "react-icons/fa";
+
+const NotFoundPage = () => {
+  const router = useRouter();
+
+  const handleGoBack = () => {
+    router.back();
+  };
+
+  return (
+    <div
+      style={{ backgroundColor: colors.signature, color: colors.white }}
+      className="flex flex-col gap-4 items-center justify-center h-screen"
+    >
+      <FaExclamationTriangle size={36} style={{ color: colors.warning }} />
+      <h1 className="text-4xl font-bold text-white">
+        페이지를 찾을 수 없습니다!
+      </h1>
+      <p className="mb-4">요청하신 페이지가 존재하지 않습니다.</p>
+      <Button
+        mode="md"
+        content="이전 페이지로 돌아가기"
+        onClick={handleGoBack}
+        color="sub"
+        customStyle={{ color: colors.signature }}
+      />
+    </div>
+  );
+};
+
+export default NotFoundPage;
