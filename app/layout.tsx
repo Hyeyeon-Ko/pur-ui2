8a4d63ea44044328ae8e04ab9b7a347278246175
gaia <gaia@kmi.or.kr>
2024-10-24 07:10:47 +0900

    Revert: 다크모드 롤백

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { ThemeProvider } from "@/components/ThemeProvider";
 
 export const metadata: Metadata = {
   title: "구매팀",
@@ -13,10 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body>
-        <ThemeProvider>{children}</ThemeProvider>
-      </body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   );
 }
--- components/layouts/Navbar.tsx
+++ components/layouts/Navbar.tsx
@@ -1,14 +1,13 @@
 import { useEffect, useState } from "react";
-import { useTheme } from "next-themes";
 import Button from "../ui/atoms/button/Button";
 import Link from "next/link";
 import Label from "../ui/atoms/label/Label";
 import { RiLogoutCircleRLine } from "react-icons/ri";
 
 const Navbar = () => {
-  const { theme, setTheme } = useTheme();
   const [user, setUser] = useState(null);
 
+  // 클라이언트에서만 실행되는 코드
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
     if (storedUser) {
@@ -17,17 +16,14 @@ const Navbar = () => {
   }, []);
 
   const handleLogout = () => {
+    // 로컬스토리지에서 사용자 정보 삭제
     localStorage.removeItem("user");
+    // 사용자 상태 업데이트
     setUser(null);
-  };
-
-  const toggleTheme = () => {
-    setTheme(theme === "dark" ? "light" : "dark");
   };
 
   return (
     <nav className="p-1 mx-4 flex justify-end items-center">
-      <Button mode="lg" onClick={toggleTheme} content="모드변경" />
       {user ? (
         <div className="flex gap-4 items-center">
           <Label content={user.employeeId} mode="lg" />
