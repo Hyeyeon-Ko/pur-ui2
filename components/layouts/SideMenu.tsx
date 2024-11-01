dffd15c75d66f69d6a59abc90a0d828ddb15543f
gaia <gaia@kmi.or.kr>
2024-11-01 11:16:54 +0900

    Style: 사이드바 ui수정

--- components/layouts/SideMenu.tsx
+++ components/layouts/SideMenu.tsx
@@ -6,13 +6,11 @@ import { MdCalculate } from "react-icons/md";
 import { RiSettings5Fill } from "react-icons/ri";
 import { FaFileContract } from "react-icons/fa";
 import { RiAdminFill } from "react-icons/ri";
-import {
-  TbSquareNumber1Filled,
-  TbSquareNumber2Filled,
-  TbSquareNumber3Filled,
-} from "react-icons/tb";
+import { TbSquareNumber1Filled } from "react-icons/tb";
+import { TbSquareNumber2Filled } from "react-icons/tb";
+import { TbSquareNumber3Filled } from "react-icons/tb";
 import Link from "next/link";
-import colors from "@/styles/colors"; // colors를 Tailwind 색상으로 변환할 수 있다면 수정 필요
+import colors from "@/styles/colors";
 import { useRouter } from "next/navigation";
 
 const SideMenu = ({
@@ -86,7 +84,7 @@ const SideMenu = ({
           icon: RiSettings5Fill,
           label: "SETTINGS",
           href: "/settings",
-          visible: ["admin", "master"],
+          visible: ["admin", "master", "admin"],
           tooltip: "설정",
         },
       ],
@@ -95,16 +93,15 @@ const SideMenu = ({
 
   return (
     <div
-      className={`bg-[${
-        colors.signature
-      }] rounded-tr-lg rounded-br-lg min-h-screen ${
+      style={{ backgroundColor: colors.signature }}
+      className={`rounded-tr-lg rounded-br-lg min-h-screen ${
         isOpen ? "w-60" : "w-16"
       } duration-300 transition-all relative`}
     >
       <div className="p-3 flex justify-end">
         <HiMenuAlt3
           size={26}
-          className="fill-white cursor-pointer"
+          style={{ fill: colors.white, cursor: "pointer" }}
           onClick={toggleSidebar}
         />
       </div>
@@ -112,7 +109,10 @@ const SideMenu = ({
       <div className="mt-4 flex flex-col gap-4">
         {menuItems.map((section) => (
           <div className="flex flex-col gap-2" key={section.title}>
-            <span className={`text-white m-4 ${isOpen ? "block" : "hidden"}`}>
+            <span
+              style={{ color: colors.white }}
+              className={`text-[#ffffff] m-4 ${isOpen ? "block" : "hidden"}`}
+            >
               {section.title}
             </span>
 
@@ -120,24 +120,55 @@ const SideMenu = ({
               <div
                 key={item.label}
                 className="relative"
+                // css로 도전!
                 onMouseEnter={() => setHoveredLabel(item.label)}
                 onMouseLeave={() => setHoveredLabel(null)}
               >
+                {/* 코드조회는 href 없이 클릭 시 토글 */}
                 <div
-                  className={`relative flex items-center justify-start text-gray-500 py-2 mx-2 rounded-md transition-colors duration-200 cursor-pointer hover:bg-sub hover:text-white`}
+                  className={`relative flex items-center justify-start text-gray-500 py-2 mx-2 rounded-md transition-colors duration-200 cursor-pointer`}
+                  style={{ backgroundColor: "transparent" }}
                   onClick={() =>
                     item.onClick ? item.onClick() : router.push(item.href)
                   }
+                  onMouseEnter={(e) => {
+                    e.currentTarget.style.backgroundColor = colors.sub;
+                    const svgElement = e.currentTarget.querySelector("svg");
+                    const spanElement = e.currentTarget.querySelector("span");
+
+                    if (svgElement) {
+                      svgElement.style.fill = colors.white;
+                    }
+
+                    if (spanElement) {
+                      spanElement.style.color = colors.white;
+                    }
+                  }}
+                  onMouseLeave={(e) => {
+                    e.currentTarget.style.backgroundColor = "transparent";
+                    const svgElement = e.currentTarget.querySelector("svg");
+                    const spanElement = e.currentTarget.querySelector("span");
+
+                    if (svgElement) {
+                      svgElement.style.fill = colors.white;
+                    }
+
+                    if (spanElement) {
+                      spanElement.style.color = colors.white;
+                    }
+                  }}
                 >
                   <item.icon
                     size={20}
-                    className={`text-white transition-transform duration-300 cursor-pointer ${
+                    style={{ fill: colors.white }}
+                    className={`transition-transform duration-300 cursor-pointer ${
                       isOpen ? "m-2" : "mx-auto"
                     }`}
                   />
 
                   <span
-                    className={`text-white font-medium p-1 mt-1 ${
+                    style={{ color: colors.white, fontWeight: "500" }}
+                    className={`p-1 mt-1 text-[#ffffff] ${
                       isOpen ? "inline-block" : "hidden"
                     }`}
                   >
@@ -145,9 +176,30 @@ const SideMenu = ({
                   </span>
 
                   {!isOpen && hoveredLabel === item.label && (
-                    <span className="bg-sub text-white text-xs p-1 rounded-md absolute left-16 top-1/2 transform -translate-y-1/2 w-40 text-center">
+                    <span
+                      style={{
+                        backgroundColor: colors.sub,
+                        color: colors.white,
+                        fontSize: "12px",
+                        padding: "4px 8px",
+                        borderRadius: "8px",
+                        position: "absolute",
+                        left: "68px",
+                        top: "50%",
+                        transform: "translateY(-50%)",
+                        width: "65px",
+                        textAlign: "center",
+                      }}
+                      className="tooltip-bubble relative"
+                    >
                       {item.tooltip}
-                      <span className="absolute left-[-10px] top-1/2 transform -translate-y-1/2 w-0 h-0 border-l-[8px] border-l-transparent border-b-[8px] border-b-transparent border-r-[8px] border-r-sub"></span>
+
+                      <span
+                        className="absolute left-[-10px] top-1/2 transform -translate-y-1/2 w-0 h-0 border-l-[8px] border-l-transparent border-b-[8px] border-b-transparent border-r-[8px] border-r-sub"
+                        style={{
+                          borderColor: `transparent transparent transparent ${colors.sub}`,
+                        }}
+                      ></span>
                     </span>
                   )}
                 </div>
@@ -159,13 +211,46 @@ const SideMenu = ({
                       <Link
                         key={subItem.label}
                         href={subItem.href}
-                        className={`flex items-center text-gray-500 py-1 mx-2 rounded-md transition-colors duration-200 hover:bg-sub hover:text-white`}
+                        className={`flex items-center text-gray-500 py-1 mx-2 rounded-md transition-colors duration-200`}
+                        onMouseEnter={(e) => {
+                          e.currentTarget.style.backgroundColor = colors.sub;
+                          const svgElement =
+                            e.currentTarget.querySelector("svg");
+                          const spanElement =
+                            e.currentTarget.querySelector("span");
+
+                          if (svgElement) {
+                            svgElement.style.fill = colors.white;
+                          }
+
+                          if (spanElement) {
+                            spanElement.style.color = colors.white;
+                          }
+                        }}
+                        onMouseLeave={(e) => {
+                          e.currentTarget.style.backgroundColor = "transparent";
+                          const svgElement =
+                            e.currentTarget.querySelector("svg");
+                          const spanElement =
+                            e.currentTarget.querySelector("span");
+
+                          if (svgElement) {
+                            svgElement.style.fill = colors.white;
+                          }
+
+                          if (spanElement) {
+                            spanElement.style.color = colors.white;
+                          }
+                        }}
                       >
                         <subItem.icon
                           size={16}
-                          className={`text-white transition-transform duration-300 cursor-pointer m-2`}
+                          style={{ fill: colors.white }}
+                          className={`transition-transform duration-300 cursor-pointer m-2`}
                         />
-                        <span className="text-white">{subItem.label}</span>
+                        <span style={{ color: colors.white }}>
+                          {subItem.label}
+                        </span>
                       </Link>
                     ))}
                   </div>
