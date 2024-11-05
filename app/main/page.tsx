bded15719f513ae2fdd1d775e36733afd394ee7b
gaia <gaia@kmi.or.kr>
2024-11-05 09:44:46 +0900

    Fix: 빌드오류 수정

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -12,81 +12,6 @@ import { FaFileContract, FaFileDownload } from "react-icons/fa";
 import { RiCheckboxMultipleLine } from "react-icons/ri";
 import { FiTool } from "react-icons/fi";
 
-export const cardData = [
-  {
-    href: "/contract",
-    icon: FaFileContract,
-    label: "계약관리",
-    bgColor: "bg-signature",
-  },
-  { href: "/tender", icon: MdCalculate, label: "입찰관리", bgColor: "bg-sub" },
-  { href: "/page-d", icon: FiTool, label: "장비관리", bgColor: "bg-signature" },
-  {
-    href: "/category/major",
-    icon: RiCheckboxMultipleLine,
-    label: "코드조회",
-    bgColor: "bg-sub",
-  },
-  {
-    href: "/page-d",
-    icon: FaFileDownload,
-    label: "매뉴얼 다운로드",
-    bgColor: "bg-signature",
-  },
-];
-
-export const deadlineItems = [
-  {
-    id: 1,
-    href: "/main/30",
-    title: "30일 이내",
-    count: 1,
-    gradientClass: "bg-Red_Darken_1",
-  },
-  {
-    id: 2,
-    href: "/main/60",
-    title: "60일 이내",
-    count: 3,
-    gradientClass: "bg-gradient_1",
-  },
-  {
-    id: 3,
-    href: "/main/90",
-    title: "90일 이내",
-    count: 10,
-    gradientClass: "bg-gradient_2",
-  },
-  {
-    id: 4,
-    href: "/main/120",
-    title: "120일 이내",
-    count: 3,
-    gradientClass: "bg-gradient_3",
-  },
-  {
-    id: 5,
-    href: "/main/180",
-    title: "180일 이내",
-    count: 20,
-    gradientClass: "bg-gradient_4",
-  },
-  {
-    id: 6,
-    href: "/main/365",
-    title: "365일 이내",
-    count: 10,
-    gradientClass: "bg-gradient_5",
-  },
-  {
-    id: 7,
-    href: "/main/expired",
-    title: "계약만료",
-    count: 300,
-    gradientClass: "bg-Grey_Default",
-  },
-];
-
 const Dashboard = () => {
   const [user, setUser] = useState(null);
 
@@ -95,6 +20,91 @@ const Dashboard = () => {
     const userData = storedUser ? JSON.parse(storedUser) : null;
     setUser(userData);
   }, []);
+
+  const cardData = [
+    {
+      href: "/contract",
+      icon: FaFileContract,
+      label: "계약관리",
+      bgColor: "bg-signature",
+    },
+    {
+      href: "/tender",
+      icon: MdCalculate,
+      label: "입찰관리",
+      bgColor: "bg-sub",
+    },
+    {
+      href: "/page-d",
+      icon: FiTool,
+      label: "장비관리",
+      bgColor: "bg-signature",
+    },
+    {
+      href: "/category/major",
+      icon: RiCheckboxMultipleLine,
+      label: "코드조회",
+      bgColor: "bg-sub",
+    },
+    {
+      href: "/page-d",
+      icon: FaFileDownload,
+      label: "매뉴얼 다운로드",
+      bgColor: "bg-signature",
+    },
+  ];
+
+  const deadlineItems = [
+    {
+      id: 1,
+      href: "/main/30",
+      title: "30일 이내",
+      count: 1,
+      gradientClass: "bg-Red_Darken_1",
+    },
+    {
+      id: 2,
+      href: "/main/60",
+      title: "60일 이내",
+      count: 3,
+      gradientClass: "bg-gradient_1",
+    },
+    {
+      id: 3,
+      href: "/main/90",
+      title: "90일 이내",
+      count: 10,
+      gradientClass: "bg-gradient_2",
+    },
+    {
+      id: 4,
+      href: "/main/120",
+      title: "120일 이내",
+      count: 3,
+      gradientClass: "bg-gradient_3",
+    },
+    {
+      id: 5,
+      href: "/main/180",
+      title: "180일 이내",
+      count: 20,
+      gradientClass: "bg-gradient_4",
+    },
+    {
+      id: 6,
+      href: "/main/365",
+      title: "365일 이내",
+      count: 10,
+      gradientClass: "bg-gradient_5",
+    },
+    {
+      id: 7,
+      href: "/main/expired",
+      title: "계약만료",
+      count: 300,
+      gradientClass: "bg-Grey_Default",
+    },
+  ];
 
   return (
     <div className="flex flex-col">
