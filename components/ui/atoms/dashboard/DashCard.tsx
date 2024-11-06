b04213eace9fe578a419235d85f39d5bfb2d2136
gaia <gaia@kmi.or.kr>
2024-11-07 08:41:00 +0900

    Feat: 준비중 노티 추가

--- components/ui/atoms/dashboard/DashCard.tsx
+++ components/ui/atoms/dashboard/DashCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import Link from "next/link";
 import { IconType } from "react-icons";
+import Toast, { ToastType } from "@/components/commons/Toast";
+import { useRouter } from "next/navigation";
 
 interface DashCardProps {
   href: string;
@@ -15,19 +16,29 @@ const DashCard: React.FC<DashCardProps> = ({
   label,
   bgColor,
 }) => {
+  const router = useRouter();
+
+  const handleClick = (event: React.MouseEvent) => {
+    if (label === "장비관리") {
+      event.preventDefault();
+      Toast.notify("해당 페이지는 준비중 입니다!", ToastType.INFO);
+    } else {
+      router.push(href);
+    }
+  };
+
   return (
-    <Link href={href}>
-      <div
-        className={`flex rounded-2xl shadow-lg p-10 ${bgColor} text-dark-Table_header cursor-pointer hover:bg-gray-100 transition hover:scale-105 hover:shadow-xl transform`}
-      >
-        <div className="flex items-center gap-6">
-          <div className="text-4xl">
-            <Icon />
-          </div>
-          <div className="text-lg text-gray-500">{label}</div>
+    <div
+      onClick={handleClick}
+      className={`flex rounded-2xl shadow-lg p-10 ${bgColor} text-dark-Table_header cursor-pointer hover:bg-gray-100 transition hover:scale-105 hover:shadow-xl transform`}
+    >
+      <div className="flex items-center gap-6">
+        <div className="text-4xl">
+          <Icon />
         </div>
+        <div className="text-lg text-gray-500">{label}</div>
       </div>
-    </Link>
+    </div>
   );
 };
 
