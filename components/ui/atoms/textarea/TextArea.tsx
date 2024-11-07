6dc38815c4a1bfc4514129c8fb6953a0ad368ef7
gaia <gaia@kmi.or.kr>
2024-11-07 13:43:53 +0900

    🔥 사용하지 않는 속성 제거

--- components/ui/atoms/textarea/TextArea.tsx
+++ components/ui/atoms/textarea/TextArea.tsx
@@ -1,29 +1,15 @@
-// components/TextArea.tsx
 import React, { TextareaHTMLAttributes } from "react";
 
-interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
-  label?: string;
-  errorMessage?: string;
-}
-
-const TextArea: React.FC<TextAreaProps> = ({
-  label,
-  errorMessage,
+const TextArea: React.FC<TextareaHTMLAttributes<HTMLTextAreaElement>> = ({
   className,
   ...props
 }) => {
   return (
-    <div className={`flex flex-col space-y-2 ${className}`}>
-      {label && (
-        <label className="text-sm font-medium text-gray-700">{label}</label>
-      )}
+    <div className={`flex flex-col ${className}`}>
       <textarea
         {...props}
-        className={`w-full p-3 border border-none rounded-md bg-dark-transparent focus:outline-none ${
-          errorMessage ? "border-red-500" : ""
-        }`}
+        className="w-full p-3 rounded-md bg-dark-transparent focus:outline-none"
       />
-      {errorMessage && <p className="text-xs text-red-500">{errorMessage}</p>}
     </div>
   );
 };
