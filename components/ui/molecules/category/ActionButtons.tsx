3f0f251949a4ded6e2757099c0c9a80b63ba2bea
gaia <gaia@kmi.or.kr>
2024-11-08 15:00:24 +0900

    ⚡️ 유지보수 용이하도록 로직 수정

--- components/ui/molecules/category/ActionButtons.tsx
+++ components/ui/molecules/category/ActionButtons.tsx
@@ -1,6 +1,6 @@
-// src/components/ui/inputs/ActionButtons.tsx
 import React from "react";
 import Button from "@/components/ui/atoms/button/Button";
+import colors from "@/styles/colors";
 
 interface ActionButtonsProps {
   isEditing: boolean;
@@ -9,20 +9,62 @@ interface ActionButtonsProps {
   onEdit: () => void;
 }
 
-const ActionButtons: React.FC<ActionButtonsProps> = ({ isEditing, onSave, onRemove, onEdit }) => (
-  <div className="flex space-x-2">
-    {isEditing ? (
-      <>
-        <Button color="signature" mode="xs" content="저장" onClick={onSave} />
-        <Button color="Button_Default" mode="xs" variant="outline" content="삭제" onClick={onRemove} />
-      </>
-    ) : (
-      <>
-        <Button color="sub" mode="xs" content="수정" onClick={onEdit} />
-        <Button color="Button_Default" mode="xs" variant="outline" content="삭제" onClick={onRemove} />
-      </>
-    )}
-  </div>
-);
+interface ButtonConfig {
+  onClick: () => void;
+  color?: keyof typeof colors;
+  content: string;
+  variant?: "inline" | "outline";
+}
+
+const BUTTON_CONFIG: Record<string, ButtonConfig> = {
+  edit: {
+    color: "sub",
+    content: "수정",
+    onClick: () => {},
+  },
+  save: {
+    color: "signature",
+    content: "저장",
+    onClick: () => {},
+  },
+  remove: {
+    color: "Button_Default",
+    variant: "outline",
+    content: "삭제",
+    onClick: () => {},
+  },
+};
+
+const ActionButtons: React.FC<ActionButtonsProps> = ({
+  isEditing,
+  onSave,
+  onRemove,
+  onEdit,
+}) => {
+  const buttons: ButtonConfig[] = isEditing
+    ? [
+        { ...BUTTON_CONFIG.save, onClick: onSave },
+        { ...BUTTON_CONFIG.remove, onClick: onRemove },
+      ]
+    : [
+        { ...BUTTON_CONFIG.edit, onClick: onEdit },
+        { ...BUTTON_CONFIG.remove, onClick: onRemove },
+      ];
+
+  return (
+    <div className="flex space-x-2">
+      {buttons.map(({ color, content, variant, onClick }, index) => (
+        <Button
+          key={index}
+          color={color}
+          mode="xs"
+          variant={variant}
+          content={content}
+          onClick={onClick}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default ActionButtons;
