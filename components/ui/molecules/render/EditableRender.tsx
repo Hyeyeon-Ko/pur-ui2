ae7406441321891d3d9a9ce1914a98bf730290c7
gaia <gaia@kmi.or.kr>
2024-11-11 15:17:12 +0900

    🎨 edittable reder 분리

---
+++ components/ui/molecules/render/EditableRender.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+import SingleDatePicker from "@/components/ui/atoms/datepicker/SingleDatePicker";
+import Input from "@/components/ui/atoms/input/Input";
+import SelectBox from "@/components/ui/atoms/selectBox/Select";
+import colors from "@/styles/colors";
+import { useDarkMode } from "@/context/DarkModeContext";
+import { equipTypeOption, fieldLabel, fieldType } from "@/lib/equipDatas";
+import { RepairRow } from "@/types/euqipTypes";
+
+interface EditableRenderProps {
+  row: RepairRow;
+  field: keyof RepairRow;
+  index: number;
+  isEditing: boolean;
+  handleInputChange: <K extends keyof RepairRow>(
+    index: number,
+    field: K,
+    value: RepairRow[K]
+  ) => void;
+}
+
+const EditableRender: React.FC<EditableRenderProps> = ({
+  row,
+  field,
+  index,
+  isEditing,
+  handleInputChange,
+}) => {
+  const { isDarkMode } = useDarkMode();
+  const inputType = fieldType[field];
+
+  if (inputType === "select") {
+    return (
+      <SelectBox
+        mode="xs"
+        placeholder="선택"
+        value={row[field] as string}
+        onChange={(e) => handleInputChange(index, field, e.target.value)}
+        options={equipTypeOption}
+        customStyle={{
+          border: "none",
+          padding: "10px",
+          backgroundColor: isDarkMode ? colors["Grey_Darken-5"] : "transparent",
+          color: isDarkMode
+            ? colors["Grey_Lighten-1"]
+            : colors["Grey_Darken-5"],
+        }}
+        disabled={!isEditing}
+      />
+    );
+  } else if (inputType === "datepicker") {
+    return (
+      <SingleDatePicker
+        selectedDate={row.repairDate}
+        onDateChange={(date) => handleInputChange(index, field, date)}
+        minDate={new Date("2000-01-01")}
+      />
+    );
+  } else {
+    return (
+      <Input
+        mode="xs"
+        value={
+          typeof row[field] === "boolean"
+            ? String(row[field])
+            : row[field] instanceof Date
+            ? row[field].toLocaleDateString()
+            : row[field]
+        }
+        onChange={(e) => handleInputChange(index, field, e.target.value)}
+        customStyle={{
+          textAlign: "center",
+          border: "none",
+          outline: "none",
+          boxShadow: "none",
+          width: "100%",
+        }}
+        placeholder={fieldLabel[field as keyof RepairRow]}
+      />
+    );
+  }
+};
+
+export default EditableRender;
