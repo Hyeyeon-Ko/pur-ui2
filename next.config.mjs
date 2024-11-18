7f0baba87f37d1c018ede65a29d41e8aded9e6f2
gaia <gaia@kmi.or.kr>
2024-11-18 11:27:10 +0900

    🔧 Feat: Chunk 최하 단위 설정(성능)

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: "standalone",
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.optimization.splitChunks = {
         chunks: "all",
+        minSize: 100, // 최소 100B 이상의 청크만 분리, default: 20KB
+        minChunks: 2, // 최소 2개 이상의 파일에서 사용될 때만 청크로 분리
       };
     }
     return config;
