f2fa5ed3db671d28a907314a898cc6b08a3f2aed
gaia <gaia@kmi.or.kr>
2024-11-20 08:44:04 +0900

    build test

--- app/main/page.body.tsx
+++ app/main/page.body.tsx
@@ -25,90 +25,90 @@ const DashboardBody = () => {
     setUser(userData);
   }, []);
 
-  // const cardData = [
-  //   {
-  //     href: "/contract",
-  //     icon: FaFileContract,
-  //     label: "계약관리",
-  //     bgColor: "bg-signature"
-  //   },
-  //   {
-  //     href: "/tender",
-  //     icon: MdCalculate,
-  //     label: "입찰관리",
-  //     bgColor: "bg-sub"
-  //   },
-  //   {
-  //     href: "/equipment",
-  //     icon: FiTool,
-  //     label: "장비관리",
-  //     bgColor: "bg-signature"
-  //   },
-  //   {
-  //     href: "/category/major",
-  //     icon: RiCheckboxMultipleLine,
-  //     label: "코드조회",
-  //     bgColor: "bg-sub"
-  //   },
-  //   {
-  //     href: "/page-d",
-  //     icon: FaFileDownload,
-  //     label: "매뉴얼 다운로드",
-  //     bgColor: "bg-signature"
-  //   }
-  // ];
+  const cardData = [
+    {
+      href: "/contract",
+      icon: FaFileContract,
+      label: "계약관리",
+      bgColor: "bg-signature"
+    },
+    {
+      href: "/tender",
+      icon: MdCalculate,
+      label: "입찰관리",
+      bgColor: "bg-sub"
+    },
+    {
+      href: "/equipment",
+      icon: FiTool,
+      label: "장비관리",
+      bgColor: "bg-signature"
+    },
+    {
+      href: "/category/major",
+      icon: RiCheckboxMultipleLine,
+      label: "코드조회",
+      bgColor: "bg-sub"
+    },
+    {
+      href: "/page-d",
+      icon: FaFileDownload,
+      label: "매뉴얼 다운로드",
+      bgColor: "bg-signature"
+    }
+  ];
 
-  // const deadlineItems = [
-  //   {
-  //     id: 1,
-  //     href: "/contract",
-  //     title: "30일 이내",
-  //     count: 1,
-  //     gradientClass: "bg-Red_Darken_1"
-  //   },
-  //   {
-  //     id: 2,
-  //     href: "/contract",
-  //     title: "60일 이내",
-  //     count: 3,
-  //     gradientClass: "bg-gradient_1"
-  //   },
-  //   {
-  //     id: 3,
-  //     href: "/contract",
-  //     title: "90일 이내",
-  //     count: 10,
-  //     gradientClass: "bg-gradient_2"
-  //   },
-  //   {
-  //     id: 4,
-  //     href: "/contract",
-  //     title: "120일 이내",
-  //     count: 3,
-  //     gradientClass: "bg-gradient_3"
-  //   },
-  //   {
-  //     id: 5,
-  //     href: "/contract",
-  //     title: "180일 이내",
-  //     count: 20,
-  //     gradientClass: "bg-gradient_4"
-  //   },
-  //   {
-  //     id: 6,
-  //     href: "/contract",
-  //     title: "365일 이내",
-  //     count: 10,
-  //     gradientClass: "bg-gradient_5"
-  //   },
-  //   {
-  //     id: 7,
-  //     href: "/contract",
-  //     title: "계약만료",
-  //     count: 300,
-  //     gradientClass: "bg-Grey_Default"
-  //   }
-  // ];
+  const deadlineItems = [
+    {
+      id: 1,
+      href: "/contract",
+      title: "30일 이내",
+      count: 1,
+      gradientClass: "bg-Red_Darken_1"
+    },
+    {
+      id: 2,
+      href: "/contract",
+      title: "60일 이내",
+      count: 3,
+      gradientClass: "bg-gradient_1"
+    },
+    {
+      id: 3,
+      href: "/contract",
+      title: "90일 이내",
+      count: 10,
+      gradientClass: "bg-gradient_2"
+    },
+    {
+      id: 4,
+      href: "/contract",
+      title: "120일 이내",
+      count: 3,
+      gradientClass: "bg-gradient_3"
+    },
+    {
+      id: 5,
+      href: "/contract",
+      title: "180일 이내",
+      count: 20,
+      gradientClass: "bg-gradient_4"
+    },
+    {
+      id: 6,
+      href: "/contract",
+      title: "365일 이내",
+      count: 10,
+      gradientClass: "bg-gradient_5"
+    },
+    {
+      id: 7,
+      href: "/contract",
+      title: "계약만료",
+      count: 300,
+      gradientClass: "bg-Grey_Default"
+    }
+  ];
 
   return (
     <div className="flex flex-col ">
