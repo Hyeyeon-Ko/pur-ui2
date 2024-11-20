ad4212c407e76eb68659e81bd7bde2e6875b6804
gaia <gaia@kmi.or.kr>
2024-11-20 10:14:41 +0900

    test: npm 재설치

--- next.config.mjs
+++ next.config.mjs
@@ -10,13 +10,6 @@ const nextConfig = {
       };
     }
 
-    // JSON 파일 로더 명시적 추가
-    config.module.rules.push({
-      test: /\.json$/,
-      loader: "json-loader",
-      type: "javascript/auto", // JSON 파일을 JS 모듈처럼 처리
-    });
-
     return config;
   },
   reactStrictMode: true,
