9ee8bf2386a726840c9f781f06226a8f263ceaa0
gaia <gaia@kmi.or.kr>
2024-11-21 13:14:36 +0900

    🩹 Fix: 사이드메뉴바 라우트 로직 추가 수정

--- components/layouts/_components/MenuItem.tsx
+++ components/layouts/_components/MenuItem.tsx
@@ -1,6 +1,7 @@
 import Tooltip from "@/components/ui/atoms/tooltip/Tootip";
 import colors from "@/styles/colors";
 import SubMenuItem from "./SubMenuItem";
+import Link from "next/link";
 
 const MenuItem = ({
   item,
@@ -18,29 +19,29 @@ const MenuItem = ({
     onMouseEnter={() => setHoveredLabel(item.label)}
     onMouseLeave={() => setHoveredLabel(null)}
   >
-    <div
-      className={`relative flex items-center justify-start text-gray-500 py-2 mx-2 rounded-md transition-colors duration-200 cursor-pointer ${
-        isOpen ? "hover:bg-sub" : ""
-      }`}
-    >
-      <item.icon
-        size={20}
-        style={{ fill: colors.white }}
-        className={`transition-transform duration-300 ${
-          isOpen ? "m-2" : "mx-auto"
-        }`}
-      />
-      <span
-        className={`p-1 mt-1 text-white font-medium ${
-          isOpen ? "inline-block" : "hidden"
+    <Link href={item.href || "#"} passHref>
+      <div
+        className={`relative flex items-center justify-start text-gray-500 py-2 mx-2 rounded-md transition-colors duration-200 cursor-pointer ${
+          isOpen ? "hover:bg-sub" : ""
         }`}
       >
-        {item.label}
-      </span>
-      {!isOpen && hoveredLabel === item.label && (
-        <Tooltip text={item.tooltip} />
-      )}
-    </div>
+        <item.icon
+          size={20}
+          style={{ fill: colors.white }}
+          className={`transition-transform duration-300 ${
+            isOpen ? "m-2" : "mx-auto"
+          }`}
+        />
+        <span
+          className={`p-1 mt-1 text-white font-medium ${
+            isOpen ? "inline-block" : "hidden"
+          }`}
+        >
+          {item.label}
+        </span>
+      </div>
+    </Link>
+    {!isOpen && hoveredLabel === item.label && <Tooltip text={item.tooltip} />}
     {item.part && isOpen && (
       <SubMenuItem subItems={item.part} isOpen={isOpen} />
     )}
